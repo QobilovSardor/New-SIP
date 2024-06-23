6a6a9dde404d1801b7acd8ee0fe2d313cf711c82
SardorQobilov <sardorqobilov555@gmail.com>
2024-06-23 23:32:35 +0500

    install sass

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -36,122 +36,122 @@ const Intro = () => {
           <CustomBreadcrumbs items={pagesSimple} />
           <h2 className="title">Стандарты по структуре страниц</h2>
           <div className="intro-box">
-            <Link to="home" className="intro-card">
+            <Link to="#!" className="intro-card">
               <img src={assets.page01} alt="" />
               <Text size="s" view="secondary" className="page-info">
                 0.1 Page
               </Text>
             </Link>
             <div className="intro-cards">
-              <Link to="header-default" className="intro-card">
+              <Link to="messenger" className="intro-card">
                 <img src={assets.page1_1} alt="" />
                 <Text size="s" view="secondary" className="page-info">
-                  1.1 Header Default
+                  Мессенджер
                 </Text>
               </Link>
-              <Link to="header-menu" className="intro-card">
+              <Link to="chat-gpt" className="intro-card">
                 <img src={assets.page1_2} alt="" />
                 <Text size="s" view="secondary" className="page-info">
-                  1.2 Header and Menu
+                  ЧатGPT
                 </Text>
               </Link>
-              <Link to="header-two-panel" className="intro-card">
+              <Link to="personal-information" className="intro-card">
                 <img src={assets.page1_3} alt="" />
                 <Text size="s" view="secondary" className="page-info">
-                  1.3 Header and Two Panel
+                  Формы
                 </Text>
               </Link>
-              <Link to="navbar" className="intro-card">
+              <Link to="directory" className="intro-card">
                 <img src={assets.page1_4} alt="" />
                 <Text size="s" view="secondary" className="page-info">
-                  2.1 Navbar Default
+                  Справочники
                 </Text>
               </Link>
-              <Link to="navbar-open" className="intro-card">
+              <Link to="not-found" className="intro-card">
                 <img src={assets.page1_6} alt="" />
                 <Text size="s" view="secondary" className="page-info">
-                  2.2 Navbar Open
+                  Данные не найдены
                 </Text>
               </Link>
-              <Link to="navbar-two-panel" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page1_7} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   2.3 Navbar and Two Panel
                 </Text>
               </Link>
-              <Link to="header-navbar" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page1_8} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   3.1 Header + Navbar Default
                 </Text>
               </Link>
-              <Link to="header-open-navbar" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page1_9} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   3.2 Header + Navbar Open
                 </Text>
               </Link>
-              <Link to="header-navbar-two-panel" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page10} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   3.3 Header + Navbar and Two Panel
                 </Text>
               </Link>
-              <Link to="navbar-header-default" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page11} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   4.1 Navbar + Header Default
                 </Text>
               </Link>
-              <Link to="navbar-header-default-open" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page12} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   4.2 Navbar + Header Open
                 </Text>
               </Link>
-              <Link to="navbar-header-two-panel" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page13} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   4.3 Navbar + Header and Two Panel
                 </Text>
               </Link>
-              <Link to="navbar-header-modern" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page14} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   5.1 Navbar + Header Modern
                 </Text>
               </Link>
-              <Link to="open-navbar-header-modern" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page15} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   5.2 Navbar + Header Open
                 </Text>
               </Link>
-              <Link to="navbar-header-two-panel-modern" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page16} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   5.3 Navbar + Header and Two Panel
                 </Text>
               </Link>
-              <Link to="other-fixed-width" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page17} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   6.1 Other - Fixed width
                 </Text>
               </Link>
-              <Link to="other-overlay-content" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page18} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   6.2 Other - Overlay content
                 </Text>
               </Link>
-              <Link to="other-block-container" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page19} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   6.2 Other - Overlay content
                 </Text>
               </Link>
-              <Link to="open-panel-or-modal" className="intro-card">
+              <Link className="intro-card" to='#!'>
                 <img src={assets.page1_6} alt="" />
                 <Text size="s" view="secondary" className="page-info">
                   7.1 Other - Open Panel or Modal
